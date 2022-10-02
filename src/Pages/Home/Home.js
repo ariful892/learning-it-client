4e78da7ecd04d0bf1381d23b5e1b4aeb4b6abeb0
ariful892 <ariful00892@gmail.com>
2022-10-02 17:02:55 +0600

    navbar added

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='bg-primary text-white'>
+            <h2>This is home</h2>
+        </div>
+    );
+};
+
+export default Home;
